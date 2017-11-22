d63c3e894897562f62235f89f91522e666d6c35a
Florian Herlicq <fherlicq@gmail.com>
2017-11-22 14:28:24 +0100

    Added a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Cleaning Seeds'
+Restaurant.destroy_all
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    "name" =>   Faker::StarTrek.character,
+    "address" =>  Faker::Lorem.sentence(3, true, 4),
+    "category" => ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    "phone_number" => Faker::Number.number(10)
+  )
+  restaurant.save!
+end
+puts 'Finished!'
